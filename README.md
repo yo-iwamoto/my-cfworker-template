3d8d08f1d04bdff1f654e85a430d62777eaa6ec1
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-12-07 20:30:37 +0900

    impl: image post

--- README.md
+++ README.md
@@ -10,3 +10,25 @@ bun run dev
 # deploy
 bun run deploy
 ```
+
+## D1
+
+```bash
+# create
+# and you should first change the names on package.json, wrangler.toml
+bun wrangler d1 create {name}
+
+# run migration
+bun run d1:migrate
+
+# run migration (on local)
+bun run d1:migrate --local
+```
+
+## R2
+
+```bash
+# create
+# and you should first change the name on wrangler.toml
+bun wrangler r2 bucket create {name}
+```
--- src/index.tsx
+++ src/index.tsx
@@ -3,54 +3,114 @@ import { validator } from 'hono/validator';
 
 type Bindings = {
   DB: D1Database;
+  BUCKET: R2Bucket;
+  BUCKET_PUBLIC_URL: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('/', async (c) => {
   const stmt = c.env.DB.prepare(`SELECT * FROM comments LIMIT 10`);
-  const { results: comments } = await stmt.all();
+  const { results: comments } = await stmt.all<{
+    id: number;
+    content: string;
+    image_key: string | null;
+  }>();
 
   return c.html(
     <main>
       <h1>Comments</h1>
 
       {comments.length > 0 ? (
-        <ul>
+        <ul style={{ listStyle: 'none' }}>
           {comments.map((comment) => (
-            <li key={comment.id}>{comment.content}</li>
+            <li key={comment.id}>
+              {comment.image_key !== null ? (
+                <img
+                  height={200}
+                  width={200}
+                  src={`${c.env.BUCKET_PUBLIC_URL}/comments/${comment.id}/${comment.image_key}`}
+                  style={{ objectFit: 'cover' }}
+                />
+              ) : null}
+              <p>{comment.content}</p>
+
+              <form action={`/comments/${comment.id}/delete`} method='post'>
+                <button type='submit'>Delete</button>
+              </form>
+            </li>
           ))}
         </ul>
       ) : (
         <p>No comment yet</p>
       )}
 
-      <form action='/comments' method='POST'>
-        <input type='text' name='content' required autofocus />
+      <form action='/comments' method='post' enctype='multipart/form-data'>
+        <textarea
+          rows={5}
+          style={{ width: 500 }}
+          name='content'
+          required
+          autofocus
+        />
+        <input type='file' name='image' accept='image/*' />
         <button type='submit'>Post</button>
       </form>
     </main>,
   );
 });
 
+app.post('/comments/:id/delete', async (c) => {
+  const { id } = c.req.param();
+  const result = await c.env.DB.prepare(`DELETE FROM comments WHERE id = ?`)
+    .bind(id)
+    .run();
+  if (result.error !== undefined) {
+    return c.json({ message: 'Internal Server Error' }, 500);
+  }
+
+  return c.redirect('/');
+});
+
 app.post(
   '/comments',
   validator('form', (value, c) => {
-    const content = value['content'];
+    const { content, image } = value;
     if (typeof content !== 'string') {
-      return c.text('Invalid request', 400);
+      return c.text('content must be a string');
+    }
+
+    if (image !== '' && !(image instanceof File)) {
+      return c.text('image must be a file');
     }
 
     return {
       content,
+      image,
     };
   }),
   async (c) => {
-    const { content } = c.req.valid('form');
+    const { content, image } = c.req.valid('form');
 
-    await c.env.DB.prepare(`INSERT INTO comments (content) VALUES (?)`)
-      .bind(content)
-      .run();
+    const res = await c.env.DB.prepare(
+      `INSERT INTO comments (content, image_key) VALUES (?, ?) RETURNING id`,
+    )
+      .bind(content, image instanceof File ? image.name : null)
+      .first<{ id: number }>();
+    if (res === null) {
+      return c.json({ message: 'Internal Server Error' }, 500);
+    }
+
+    if (image instanceof File) {
+      const putResult = await c.env.BUCKET.put(
+        `comments/${res.id}/${image.name}`,
+        image,
+      );
+      console.log(putResult);
+      if (putResult === null) {
+        return c.json({ message: 'Internal Server Error' }, 500);
+      }
+    }
 
     return c.redirect('/');
   },
